e570d5f653fd1f604110b746bc27e54b426b65e1
Madhav Gautam <gautam.madhav63@gmail.com>
2025-04-23 14:53:25 +0530

    Initialize sign-up popup, form, and close button after page load

---
+++ signup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const signUpButton = document.getElementById('sign');
+    const signUpPopup = document.getElementById('signUpPopup');
+    const closeButton = signUpPopup.querySelector('.close');
+    const signUpForm = document.getElementById('signUpForm');
+})
