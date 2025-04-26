5eebc085af1f6eee503785714b8edf58ff53de26
Vaibhav Katyal <vaibhav0480.becse24@chitkara.edu.in>
2025-04-26 20:02:32 +0530

    Made the variable for selecting the cards and contents of the page to change the content as it clicked

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 
+const cards = document.querySelectorAll('.pt');
+const contents = {
+    'why-heart': document.querySelector('.wellnesstracking'),
+    'why-fitness': document.querySelector('.fitnessgoals'),
+    'why-weight': document.querySelector('.weightmngmnt'),
+    'why-health': document.querySelector('.healthassm')
+};
   
