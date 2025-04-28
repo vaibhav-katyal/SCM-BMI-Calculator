92cc3f7be49d918a38bf7684d18806041c8b82e1
Vaibhav Katyal <vaibhav0480.becse24@chitkara.edu.in>
2025-04-28 10:33:00 +0530

    Added the variables to access the buttons and hamburger

---
+++ navbar.js
@@ -0,0 +1,6 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the hamburger menu and navigation elements
+    const hamburger = document.querySelector('.hamburger');
+    const center = document.querySelector('.center');
+})
